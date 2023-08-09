1584dd43a4e16469d31dcd9ba1c932cccecc10b5
Matheus Maydana <mattmaydana@gmail.com>
2023-08-09 13:34:13 -0300

    começando com header

--- src/Templates/Layout/Header/Header.blade.php
+++ src/Templates/Layout/Header/Header.blade.php
@@ -6,14 +6,18 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="robots" content="noindex, noarchive">
         <meta name="format-detection" content="telephone=no">
-        <base href="https://programador.dev">
+        <base href="{{$dominio}}">
         <link rel="stylesheet" href="/css/site.min.css">
         <title>Index</title>
     </head>
     <body>
         <header>
             <div>
-                <p>Header</p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui vitae ex voluptas consectetur, voluptates minus corporis laudantium minima itaque nemo corrupti similique harum delectus dolorem libero ut maxime nisi culpa.
+                <div>
+                    <img class="img" src="https://avatars.githubusercontent.com/u/36517599" alt="https://github.com/19950512" title="Github 19950512" />
+                </div>
+                <div><a href="/">Início</a></div>
+                <div><a href="/sobre">Sobre</a></div>
+                <div><a href="/contato">Contato</a></div>
             </div>
         </header>
